b57738719a7561bec2b631932d7a83f58cf7ead2
KamnaDhingra <kdhingra@finvasia.in>
2024-05-17 15:53:46 +0530

    some changes

--- ops.py
+++ ops.py
@@ -1,13 +1,8 @@
-def add(x,y):
-    print("result of addition",  x+y)
-    return x+y
+statements = [
+    "This is the first statement.",
+    "This is the second statement.",
+    "This is the third statement."
+]
 
-def subtract(x,y):
-    return x-y
-
-def multiply(x,y):
-    print("result of addition",  x*y)
-    return x*y
-
-def divide(x,y):
-    return x/y
+for statement in statements:
+    print(statement)
