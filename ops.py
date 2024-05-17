a46ef17b8fe58c50784aa7907b34a184136a9cf2
KamnaDhingra <kdhingra@finvasia.in>
2024-05-17 15:17:42 +0530

    python_sample project

--- ops.py
+++ ops.py
@@ -1,10 +1,12 @@
 def add(x,y):
+    print("result of addition",  x+y)
     return x+y
 
 def subtract(x,y):
     return x-y
 
 def multiply(x,y):
+    print("result of addition",  x*y)
     return x*y
 
 def divide(x,y):
